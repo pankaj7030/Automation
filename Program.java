26f3744c667d3dcec61ac77f5252cf9264434d49
Pankaj <pankajdeshmukh507@gmail.com>
2023-01-09 16:51:08 +0530

    Second commit

---
+++ Program.java
@@ -0,0 +1,32 @@
+public class Program{
+
+public static void main(String[]arg){
+
+Program.t1();
+Program b1=new Program();
+b1.t2();
+b1.t3();
+
+System.out.println("Java 19 first program");
+}
+
+public static void t1(){
+System.out.println("This is static method");
+
+}
+public void t2(){
+System.out.println("This is non static method");
+}
+
+public void t3(){
+int a=20;
+int b=30;
+int c=a+b;
+int d=b-a;
+float e=b/a;
+int f=a*b;
+
+System.out.println("Addition="+c",""Substraction="+d);
+}
+
+}
